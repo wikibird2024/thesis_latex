f0c13c9eb9d52b98a77ad9565fa319497cfb34fa
hx <tranhaobku2017@gmail.com>
2025-08-13 14:19:25 +0700

    kha hon ban truoc mot chut khong loi linh tinh nhieu, bang van loi

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@
 MAIN_FILE="main"
 
 # Remove common auxiliary files
-rm -f "$MAIN_FILE.aux" ""$MAIN_FILE.bbl" $MAIN_FILE.bcf" "$MAIN_FILE.blg" "$MAIN_FILE.lof" "$MAIN_FILE.log" "$MAIN_FILE.lot" "$MAIN_FILE.out" "$MAIN_FILE.toc" "$MAIN_FILE.run.xml"
+rm -f "$MAIN_FILE.aux" "$MAIN_FILE.bbl" "$MAIN_FILE.bcf" "$MAIN_FILE.blg" "$MAIN_FILE.lof" "$MAIN_FILE.log" "$MAIN_FILE.lot" "$MAIN_FILE.out" "$MAIN_FILE.toc" "$MAIN_FILE.run.xml"
 
 # Remove other common LaTeX-generated files
 rm -f *.spl *.synctex.gz *.fls *.fdb_latexmk
