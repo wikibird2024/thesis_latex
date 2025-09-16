f53505f73f8af8182f8db7b2738a3dd60358f808
hx_main <tranhaobku2017@gmail.com>
2025-09-16 17:04:00 +0700

    sua lai file bild nhanh them hinh media keypoint

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
+
 #!/bin/bash
+# Xóa file tạm
 rm -f *.aux *.log *.out *.toc *.bbl *.bcf *.blg *.run.xml *.nav *.snm *.synctex.gz
-xelatex --shell-escape main.tex
-biber main
-xelatex --shell-escape main.tex
-xelatex --shell-escape main.tex
-zathura main.pdf
+
+# Build LaTeX
+xelatex -shell-escape -interaction=nonstopmode main.tex &
+pid1=$!
+
+biber main &
+pid2=$!
+
+# Chờ các tiến trình xong
+wait $pid1
+wait $pid2
+
+# Build lại để cập nhật cross-ref
+xelatex -shell-escape -interaction=nonstopmode main.tex
+xelatex -shell-escape -interaction=nonstopmode main.tex
+
+# Mở PDF
+zathura main.pdf &
