5689d88242ef1bb21bbfd214439ccee6405d14b2
hx_main <tranhaobku2017@gmail.com>
2025-10-24 13:49:33 +0700

    sua ti

--- test.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-rm -f *.aux *.log *.out *.toc *.bbl *.bcf *.blg *.run.xml *.nav *.snm *.synctex.gz
-xelatex --shell-escape test.tex
-biber test
-xelatex --shell-escape test.tex
-xelatex --shell-escape test.tex
-zathura test.pdf
